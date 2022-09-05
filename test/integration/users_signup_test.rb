699feece50a8abcb643a36dbcfe59c4a32f09794
Hiro-Nagai <gm.komabun2003@gmail.com>
2022-09-05 17:33:51 +0000

    Implement basic login

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -13,6 +13,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     follow_redirect!
     assert_template 'users/show'
+    assert is_logged_in?
   end
   
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,11 +6,16 @@ Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
   # 特定のワーカーではテストをパラレル実行する
-  parallelize(workers: :number_of_processors)
+  # parallelize(workers: :number_of_processors)
 
   # すべてのテストがアルファベット順に実行されるよう、
   #test/fixtures/*.ymlにあるすべてのfixtureをセットアップする
   fixtures :all
+  
+  # テストユーザーがログイン中の場合にtrueを返す
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
 
   # （すべてのテストで使うその他のヘルパーメソッドは省略）
 end
